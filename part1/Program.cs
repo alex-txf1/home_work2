42a2b70103d0c51b6de3782cbe4d6547009d8634
Alex <alex-txf1@mail.ru>
2022-06-29 16:18:42 +0300

    первая задача

--- part1/Program.cs
+++ part1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("вывод произвального трехзначного числа");
+Console.WriteLine("_______________________________");
+int start = 100;
+int stop = 999;
+int A = secondNumber(start , stop);
+Console.WriteLine("вторая цифра  " + A);
+
+int secondNumber(int start , int stop)
+{    
+    int number = new Random().Next(start , stop);
+    Console.WriteLine(number);
+    int secnum = number % 100;
+    int secnum1 = secnum / 10;
+    return secnum1;
+}
