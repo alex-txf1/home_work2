1e8295167fe813bc7acdcf2be412ea00e5f0808f
Alex <alex-txf1@mail.ru>
2022-07-01 10:33:17 +0300

    вторая задача

---
+++ part2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("вывод третьего числа из произвольного числа");
+Console.WriteLine("_______________________________");
+int numbers = new Random().Next(0 , 200);
+Console.WriteLine(numbers);
+int A = 100;
+found(numbers);
+void found(int numbers)
+{   
+    if(numbers > A) 
+    {
+    string str = numbers . ToString();
+    Console.WriteLine("третье число  "+str[2]);
+    }  
+    else{
+        Console.Write("в числе "+numbers+ " нет цифры под третьим номером");
+    }
+}
+    
